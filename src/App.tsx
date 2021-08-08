fc0adee94ec38de7878c1ff84e812016ff6b0b0d
zorro901 <zoro907@gmail.com>
2021-08-09 05:01:32 +0900

    59. カスタムフック作成

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,7 @@ import { useState } from "react";
 import { UserProfile } from "./types/userProfile";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchUser = () => {};
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+import { UserProfile } from "../types/userProfile";
+import { User } from "../types/api/user";
+
+// 全ユーザーの一覧を取得するカスタムフック
+export const useAllUsers = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  return { getUsers, userProfiles, loading, error };
+};
