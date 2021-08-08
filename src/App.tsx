4e4d421b5cbc2b075ae2258976bab68ea0488392
zorro901 <zoro907@gmail.com>
2021-08-09 05:09:01 +0900

    60. カスタムフックを呼び出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const onClickFetchUser = () => {};
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => getUsers();
+
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
